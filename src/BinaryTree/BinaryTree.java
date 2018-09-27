70383e7be552ddfd3fc675ffa964a76e57603fe9
Riat <Harpal.Riat@carsarrive.com>
2018-09-26 18:08:54 -0700

    DFT and BST (Traversal) added

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -1,11 +1,16 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 	
-	//int[] values = {10,7,13,9,11};
-	int[] values = new int[6];
+	int[] values = {10,7,13,9,11,22};
+	//int[] values = new int[6];
 	Node root;
 	boolean findme = false;
+	Queue<Node> q = new LinkedList<>();
+	
 	
 	private void randomArray() {
 		for (int i = 0; i < values.length; i++) {
@@ -15,7 +20,7 @@ public class BinaryTree {
 	
 	public void buildTree() {
 		
-		randomArray();
+		//randomArray();
 		
 		for(int value: values) {
 			Node node = new Node(value);
@@ -57,6 +62,60 @@ public class BinaryTree {
 		}
 	}
 	
+	/*
+	 * Breadth First Traversal
+	 */
+	public void BFStraversal(Node root) {
+		Node focusNode = root;
+		q.add(focusNode);
+		
+		while(q.isEmpty() == false) {
+			
+			focusNode = q.poll();
+			System.out.print(focusNode.value + " ");
+			if(focusNode.left != null) {
+				q.add(focusNode.left);
+			}
+			if(focusNode.right != null) {
+				q.add(focusNode.right);
+			}
+		}
+		
+	}
+	
+	/*
+	 * Depth First Traversal
+	 * InOrder Traversal
+	 * Left -> Root -> Right
+	 */
+	public void InOrderTraversal(Node root) {
+		if(root.left != null) InOrderTraversal(root.left);
+		System.out.print(root.value + " ");
+		if(root.right != null) InOrderTraversal(root.right);
+	}
+	
+	
+	/*
+	 * Depth First Traversal
+	 * PostOrder Traversal
+	 * Left -> Right -> Root
+	 */
+	public void PostOrderTraversal(Node root) {
+		if(root.left != null) PostOrderTraversal(root.left);
+		if(root.right != null) PostOrderTraversal(root.right);
+		System.out.print(root.value + " ");
+	}
+	
+	/*
+	 * Depth First Traversal
+	 * PreOrder Traversal
+	 * Root-> Left -> Right 
+	 */
+	public void PreOrderTraversal(Node root) {
+		System.out.print(root.value + " ");
+		if(root.left != null) PreOrderTraversal(root.left);
+		if(root.right != null) PreOrderTraversal(root.right);
+	}
 	
 	public boolean findValue(int value) {
             
@@ -115,13 +174,21 @@ public class BinaryTree {
 	public static void main(String[] args) {
 		BinaryTree bt = new BinaryTree();
 		bt.buildTree();
+		bt.BFStraversal(bt.root);
+		System.out.println("");
+		bt.InOrderTraversal(bt.root);
+		System.out.println("");
+		bt.PostOrderTraversal(bt.root);
+		System.out.println("");
+		bt.PreOrderTraversal(bt.root);
+		System.out.println("");
 		bt.traverse();
-		System.out.println("");
-		int findval = 5;
-		if(bt.findValue(findval)) System.out.println(findval + " Found");
-                else  System.out.println(findval + " NOT");
+		//System.out.println("");
+		//int findval = 5;
+		//if(bt.findValue(findval)) System.out.println(findval + " Found");
+                //else  System.out.println(findval + " NOT");
 		//bt.ascendingOrderTree();
-		System.out.println("");
+		//System.out.println("");
 		//bt.descendingOrderTree();
 		
 	}
